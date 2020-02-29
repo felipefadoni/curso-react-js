2eeec69f1b1e41f7f9c5a5e386c38b2ce262de96
Felipe Fadoni <felipe.fadoni@mult-e.com.br>
2020-02-29 20:35:43 -0300

    styled components axios react-bootstrap

---
+++ src/components/App/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { ContainerApp } from "./styles";
+
+import GlobalStyle from "../../styles/global";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <ContainerApp>
+        <h1>Iniciando um projeto em ReactJS</h1>
+      </ContainerApp>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/App/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { Container } from "react-bootstrap";
+
+export const ContainerApp = styled(Container)`
+  background-color: red;
+`;
